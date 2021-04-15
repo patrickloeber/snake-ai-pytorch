40c19ff1b7b8d2708ed5e30a4716c71d5b894e86
MichaelSeguin <75803989+MichaelSeguin@users.noreply.github.com>
2021-04-15 12:39:16 +0000

    Update helper.py

--- helper.py
+++ helper.py
@@ -15,3 +15,5 @@ def plot(scores, mean_scores):
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show(block=False)
+    plt.pause(.1)
